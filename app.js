2c06528d12af58e13b7245b554976449b24e3d3c
LusellyHinestroza <valencialucelly48@gmail.com>
2026-04-10 14:45:24 -0500

    luseely

---
+++ app.js
@@ -0,0 +1,9 @@
+function reproducir(){
+   let musicas =  document.getElementById("musica");
+   if (musicas.style.display === "none"){
+        musicas.style.display = "block";
+   }else{
+        musicas.style.display = "none"
+   }
+
+}
